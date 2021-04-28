e633b97a8a60438157100b9a112d7fa5ebc41603
zigazajc007 <ziga.zajc007@gmail.com>
2021-04-28 14:51:05 +0200

    Added validation to login page

---
+++ js/default-functions.js
@@ -0,0 +1,15 @@
+function hide(element){
+    document.getElementById(element).style.visibility = 'hidden';
+}
+
+function show(element){
+    document.getElementById(element).style.visibility = 'visible';
+}
+
+function setText(element, text){
+    document.getElementById(element).innerText = text;
+}
+
+function validURL(url) {
+    return new RegExp(/^(http|https):\/\/(([a-zA-Z0-9$\-_.+!*'(),;:&=]|%[0-9a-fA-F]{2})+@)?(((25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])){3})|localhost|([a-zA-Z0-9\-\u00C0-\u017F]+\.)+([a-zA-Z]{2,}))(:[0-9]+)?(\/(([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*(\/([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*)*)?(\?([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?(\#([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?)?$/).test(url);
+}
