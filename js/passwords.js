82bbfd9de5e2ebfa0dee2ca049f71dcb87a761ba
zigazajc007 <ziga.zajc007@gmail.com>
2021-05-04 22:55:33 +0200

    Finished back end for password generator

--- js/passwords.js
+++ js/passwords.js
@@ -1,3 +1,41 @@
+function updateGeneratedPassword(upper, number, special){
+    let password = "";
+    let length = document.getElementById('btn-length').value;
+    let lowers = "abcdefghijklmnopqrstuvwxyz";
+    let uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numbers = "1234567890";
+    let specials = "!@#$%?&*";
+
+    for (let i = 0; i < length; i++) password += lowers.charAt(Math.floor(Math.random() * lowers.length));
+
+    password = password.split("");
+
+    if(upper){
+        let upper_amount = Math.floor(length / 2 - Math.random() * (length / 2) + 1);
+        for(let i = 0; i < upper_amount; i++){
+            password[Math.floor(Math.random() * password.length)] = uppers.charAt(Math.floor(Math.random() * uppers.length));
+        }
+    }
+
+    if(number){
+        let number_amount = Math.floor(length / 2 - Math.random() * (length / 2) + 1);
+        for(let i = 0; i < number_amount; i++){
+            password[Math.floor(Math.random() * password.length)] = numbers.charAt(Math.floor(Math.random() * numbers.length));
+        }
+    }
+
+    if(special){
+        let special_amount = Math.floor(Math.random(3) + 1);
+        for(let i = 0; i < special_amount; i++){
+            password[Math.floor(Math.random() * password.length)] = specials.charAt(Math.floor(Math.random() * specials.length));
+        }
+    }
+
+    password = password.join("");
+
+    document.getElementById('generated-password').innerText = password;
+}
+
 function changeDialog(style, text){
     switch(style){
         case 1:
@@ -67,21 +105,36 @@ function changeDialog(style, text){
             if(document.getElementById('dialog-title').innerText != "Password Generator"){
                 const data = text.split(" ");
 
+                let btn_upper_enabled = true;
+                let btn_numbers_enabled = true;
+                let btn_special_enabled = true;
+
                 document.getElementById('dialog-icon').className = "mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10";
                 document.getElementById('dialog-icon').innerHTML = "<svg class='h-6 w-6 text-blue-600' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' aria-hidden='true'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><rect x='3' y='3' width='6' height='6' rx='1' /><rect x='15' y='15' width='6' height='6' rx='1' /><path d='M21 11v-3a2 2 0 0 0 -2 -2h-6l3 3m0 -6l-3 3' /><path d='M3 13v3a2 2 0 0 0 2 2h6l-3 -3m0 6l3 -3' /></svg>";
     
                 document.getElementById('dialog-title').innerText = "Password Generator";
 
-                document.getElementById('dialog-text').innerHTML = "<div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='availability-label'><span class='text-sm font-medium text-gray-900'>A-Z</span></span><button type='button' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='availability-label'><span class='text-sm font-medium text-gray-900'>a-z</span></span><button type='button' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='availability-label'><span class='text-sm font-medium text-gray-900'>0-9</span></span><button type='button' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='availability-label'><span class='text-sm font-medium text-gray-900'>!@#$%?&*</span></span><button type='button' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div>     <div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='availability-label'><span class='text-sm font-medium text-gray-900'>Length</span></span><input type='range' min='8' max='30' value='10' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'></div>";
+                document.getElementById('dialog-text').innerHTML = "<div class='flex items-center justify-between'><span class='flex-grow flex flex-col'><span class='text-sm font-medium text-gray-900'>A-Z</span></span><button type='button' id='btn-upper' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col'><span class='text-sm font-medium text-gray-900'>0-9</span></span><button type='button' id='btn-numbers' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col'><span class='text-sm font-medium text-gray-900'>!@#$%?&*</span></span><button type='button' id='btn-special' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500' role='switch' aria-checked='false'><span aria-hidden='true' class='pointer-events-none absolute bg-white w-full h-full rounded-md'></span><span aria-hidden='true' class='bg-gray-200 pointer-events-none absolute h-4 w-9 mx-auto rounded-full transition-colors ease-in-out duration-200'></span><span aria-hidden='true' class='translate-x-0 pointer-events-none absolute left-0 inline-block h-5 w-5 border border-gray-200 rounded-full bg-white shadow transform ring-0 transition-transform ease-in-out duration-200'></span></button></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col'><span class='text-sm font-medium text-gray-900'>Length</span></span><input type='range' id='btn-length' min='8' max='30' value='10' class='flex-shrink-0 group relative rounded-full inline-flex items-center justify-center h-5 w-10 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'></div><div class='flex items-center justify-between'><span class='flex-grow flex flex-col' id='generated-password'></span></div>";
     
                 document.getElementById('dialog-button').className = "inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:text-sm";
                 
+                updateGeneratedPassword(btn_upper_enabled, btn_numbers_enabled, btn_special_enabled);
+
+                document.getElementById('btn-upper').onclick = () => updateGeneratedPassword(btn_upper_enabled, btn_numbers_enabled, btn_special_enabled);
+                document.getElementById('btn-numbers').onclick = () => updateGeneratedPassword(btn_upper_enabled, btn_numbers_enabled, btn_special_enabled);
+                document.getElementById('btn-special').onclick = () => updateGeneratedPassword(btn_upper_enabled, btn_numbers_enabled, btn_special_enabled);
+                document.getElementById('btn-length').onchange = () => updateGeneratedPassword(btn_upper_enabled, btn_numbers_enabled, btn_special_enabled);
+
+
                 if(data[0] != null){
                     document.getElementById('dialog-button').innerText = "Use";
-                    document.getElementById('dialog-button').onclick = () => changeDialog(4, text);
+                    document.getElementById('dialog-button').onclick = () => {
+                        text = data[0] + " " + data[1] + " " + data[2] + " " + document.getElementById('generated-password').innerText;
+                        changeDialog(4, text);
+                    }
                 }else{
                     document.getElementById('dialog-button').innerText = "Copy";
-                    document.getElementById('dialog-button').onclick = () => copyToClipboard("password here");
+                    document.getElementById('dialog-button').onclick = () => copyToClipboard(document.getElementById('generated-password').innerText);
                 }
             }
         break;
@@ -259,10 +312,6 @@ function editPassword(password_id){
 
     };
     xhr.send("password_id=" + password_id + "&website=" + website + "&username=" + username + "&password=" + encodeURIComponent(password));
-}
-
-function passwordGenerator(password_id){
-
 }
 
 function refreshPasswords(){
