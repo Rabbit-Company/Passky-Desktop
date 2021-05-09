50272679a792089c3531430f132c32d3452937e4
zigazajc007 <ziga.zajc007@gmail.com>
2021-05-09 22:58:26 +0200

    Fixed white screen bug

--- js/preload.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-
-})
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 500,
+    minWidth: 500,
+    minHeight: 500
+  });
+
+  win.setMenuBarVisibility(false);
+  win.setTitle("Passky");
+  win.setIcon(path.join(__dirname, 'images/logo.png'));
+
+  win.loadFile(path.join(__dirname, 'html/login.html'));
+  //win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
