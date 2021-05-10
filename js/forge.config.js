6a713924899bcc4e2a05fd76c183f7c3aef71456
zigazajc007 <ziga.zajc007@gmail.com>
2021-05-10 11:02:01 +0200

    Seperate forge config

---
+++ js/forge.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+    "packagerConfig": {},
+    "makers": [
+      {
+        "name": "@electron-forge/maker-squirrel",
+        "config": {
+          "name": "Passky",
+          "title": "Passky",
+          "exe": "Passky.exe",
+          "description": "Simple and secure password manager",
+          "iconUrl": "https://raw.githubusercontent.com/Rabbit-Company/Passky-Desktop/main/images/logo.ico",
+          "setupIcon": path.join(__dirname, "images/logo.ico")
+        }
+      },
+      {
+        "name": "@electron-forge/maker-zip",
+        "platforms": [
+          "darwin"
+        ]
+      },
+      {
+        "name": "@electron-forge/maker-deb",
+        "config": {
+          "options": {
+            "name": "Passky",
+            "productName": "Passky",
+            "maintainer": "Rabbit Company",
+            "homepage": "https://rabbit-company.com",
+            "description": "Simple and secure password manager",
+            "productDescription": "Passky is simple password manager, which works on a zero trust architecture. That means only user will be able to decrypt his passwords. So users can safelly store their passwords on any server. That means if a server on where all passwords are stored get hacked, hacker won't be able to decrypt passwords and data on this server will be useless for him.",
+            "genericName": "Password Manager",
+            "icon": "images/logo.png"
+          }
+        }
+      },
+      {
+        "name": "@electron-forge/maker-rpm",
+        "config": {
+          "options": {
+            "name": "Passky",
+            "productName": "Passky",
+            "maintainer": "Rabbit Company",
+            "homepage": "https://rabbit-company.com",
+            "description": "Simple and secure password manager",
+            "productDescription": "Passky is simple password manager, which works on a zero trust architecture. That means only user will be able to decrypt his passwords. So users can safelly store their passwords on any server. That means if a server on where all passwords are stored get hacked, hacker won't be able to decrypt passwords and data on this server will be useless for him.",
+            "genericName": "Password Manager",
+            "icon": "images/logo.png"
+          }
+        }
+      }
+    ]
+}
