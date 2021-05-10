8f0a67823eae5d73da323778dd7d4ff82b98ab2c
zigazajc007 <ziga.zajc007@gmail.com>
2021-05-10 10:15:03 +0200

    Added descriptions, icons... to windows, deb and rpm installers

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) return app.quit();
 
 function createWindow () {
   const win = new BrowserWindow({
