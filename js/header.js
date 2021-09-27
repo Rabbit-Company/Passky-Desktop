0f2957c53dfaed222f7035a33a210a11d3554c70
BuildTools <unconfigured@null.spigotmc.org>
2021-09-27 09:35:03 +0200

    Updated German and Polish languages

--- js/header.js
+++ js/header.js
@@ -2,19 +2,15 @@ if(localStorage.theme == null || typeof(localStorage.theme) == 'undefined') loca
 if(localStorage.lang == null || typeof(localStorage.lang) == 'undefined') localStorage.lang = "en";
 if(localStorage.sessionDuration == null || typeof(localStorage.sessionDuration) == 'undefined') localStorage.sessionDuration = 20;
 
-if(!(localStorage.theme == "dark" || localStorage.theme == "light" || localStorage.theme == "blue" || localStorage.theme == "dracula" || localStorage.theme == "gray")) localStorage.theme = "dark";
+if(!(["dark", "light", "blue", "dracula", "gray"].includes(localStorage.theme))) localStorage.theme = "dark";
+document.getElementById("css-theme").href = "css/themes/" + localStorage.theme + ".css";
 
-switch(localStorage.theme){
-    case "light":
-        document.getElementById("css-theme").href = "css/themes/light.css";
-    break;
-    case "gray":
-        document.getElementById("css-theme").href = "css/themes/gray.css";
-    break;
-    case "blue":
-        document.getElementById("css-theme").href = "css/themes/blue.css";
-    break;
-    case "dracula":
-        document.getElementById("css-theme").href = "css/themes/dracula.css";
-    break;
+document.onkeydown = function(e) {
+    if(e.key == "F12") return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'I') return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'C') return false;
+    if(e.ctrlKey && e.shiftKey && e.key == 'J') return false;
+    if(e.ctrlKey && (e.key == 'u' || e.key == 'U')) return false;
 }
+
+document.addEventListener('contextmenu', e => e.preventDefault());
